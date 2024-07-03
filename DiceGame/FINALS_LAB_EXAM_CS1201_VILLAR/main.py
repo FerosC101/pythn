9a6c154285bf87234cfe50853b2cab471768a0fc
Vince Anjo Villar <148174794+FerosC101@users.noreply.github.com>
2024-07-03 09:06:04 +0800

    Delete DiceGame/FINALS_LAB_EXAM_CS1201_VILLAR directory

--- DiceGame/FINALS_LAB_EXAM_CS1201_VILLAR/main.py
+++
@@ -1,9 +0,0 @@
-from User import User
-
-def main():
-    print('WELCOME')
-    user_instance = User("username", "password", "user_scores.txt", "User_Data")
-    user_instance.menu()
-
-if __name__ == '__main__':
-    main()
